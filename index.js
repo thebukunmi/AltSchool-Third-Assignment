b1a8e831c8de29887cc2dd7878df2c0414ed8e67
Bukunmi Abikoye <thebukunmi@gmail.com>
2023-12-19 23:01:08 +0100

    third assignment commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const openMenu = document.querySelector(".open-menu")
+
+  function showMenu(){
+      openMenu.style.left ="0%";
+      console.log("yayy")
+  }
+
+  function hideMenu(){        
+      openMenu.style.left ="-100%";        
+  }
